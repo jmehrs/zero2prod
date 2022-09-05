a73ff031fa8d1b7afef364778aac93c5d71103a0
Jai Mehra <jmjaiken@gmail.com>
2022-09-05 13:33:45 -0400

    App startup

---
+++ src/startup.rs
@@ -0,0 +1,22 @@
+use std::net::TcpListener;
+use sqlx::PgPool;
+use actix_web::{web, App, HttpServer};
+use actix_web::dev::Server;
+
+use crate::routes::{health_check, subscribe};
+
+// We need to mark `run` as public.
+// It is no longer a binary entrypoint, therefore we can mark it as async
+// without having to use any proc-macro incantation.
+pub fn run(listener: TcpListener, db_pool: PgPool) -> std::io::Result<Server> {
+    let db_pool = web::Data::new(db_pool);
+    let server = HttpServer::new(move || {
+        App::new()
+            .app_data(db_pool.clone())
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
+     Ok(server)
+}
